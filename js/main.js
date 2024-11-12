af1ae821538780e49bc54c85bb14949bb03480d3
Dongwook Lee <skyldw3@gmail.com>
2024-11-12 22:25:53 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     gsap.registerPlugin(ScrollTrigger)
-
+    
     gsap.from(".intro h2", 1.5, {
         delay: 0.8,
         rotateX: 90,
@@ -114,6 +114,171 @@ $(document).ready(function(){
         ease: 'expo.out',
     })
 
+    gsap.to(".profile", {
+        scrollTrigger: {
+            trigger: '.profile',
+            start: 'top 60%',
+            end: 'top 40%',
+            scrub: 0.5,
+        },
+        'width': '100%',
+        duration: 10,
+        ease: 'none'
+    })
+
+    gsap.to(".profile", {
+        scrollTrigger: {
+            trigger: '.profile',
+            start: '30% top',
+            end: 'bottom top',
+            scrub: 0.1,
+        },
+        opacity: 0,
+        duration: 10,
+        ease: 'none'
+    })
+
+    gsap.to(".profile_bg", {
+        scrollTrigger: {
+            trigger: '.profile',
+            start: 'top 45%',
+            end: 'top top',
+            scrub: 0.5,
+        },
+        borderRadius: '0',
+        duration: 10,
+        ease: 'none'
+    })
+
+    gsap.to(".mission h3", 0.8, {
+        scrollTrigger: {
+            trigger: '.mission h3',
+            start: 'top 60%'
+        },
+        opacity: 1,
+        filter: 'blur(0px)',
+        ease: 'power2.out'
+    })
+
+    gsap.from(".mission p", 1, {
+        scrollTrigger: {
+            trigger: '.mission h3',
+            start: 'top 60%'
+        },
+        delay: 0.2,
+        opacity: 0,
+        y: '6rem',
+        stagger: 0.1,
+        ease: 'power2'
+    })
+
+    gsap.to(".tools h3", 0.8, {
+        scrollTrigger: {
+            trigger: '.tools h3',
+            start: 'top 80%'
+        },
+        opacity: 1,
+        filter: 'blur(0px)',
+        ease: 'power2.out'
+    })
+
+    gsap.from(".carousel", 1.4, {
+        scrollTrigger: {
+            trigger: '.carousel',
+            start: '35% 80%'
+        },
+        opacity: 0,
+        ease: 'power2'
+    })
+
+    $('.carousel_item').mouseenter(function(){
+        const itemIndex = $(this).index();
+
+        if(itemIndex == 6 || itemIndex == 12){
+            $('.carousel_tooltip').html('PLUG-IN, AUTO-LAYOUT')
+        }else if(itemIndex == 7 || itemIndex == 13){
+            $('.carousel_tooltip').html('GSAP, Jquery')
+        }else if(itemIndex == 8 || itemIndex == 14){
+            $('.carousel_tooltip').html('FLEX, ANIMATION, 미디어쿼리')
+        }else if(itemIndex == 9 || itemIndex == 15){
+            $('.carousel_tooltip').html('마크업, 시맨틱 태그')
+        }else if(itemIndex == 10 || itemIndex == 16){
+            $('.carousel_tooltip').html('3D 오브젝트, 로고')
+        }else if(itemIndex == 11 || itemIndex == 17){
+            $('.carousel_tooltip').html('톤 보정, 필터, 배경지우기, 이미지 변형')
+        }else{
+            $('.carousel_tooltip').html('')
+        }
+
+        gsap.to(".carousel_tooltip", 0.2, {
+            opacity: 1,
+            scale: 1,
+            ease: 'expo.out'
+        })
+    }).mouseleave(function(){
+        gsap.to(".carousel_tooltip", 0.2, {
+            opacity: 0,
+            scale: 0,
+            ease: 'expo.out'
+        })
+    })
+
+    $('.carousel_item').mousemove(function(e){
+        const cursorX = e.pageX + 10
+        const cursorY = e.pageY + 16
+
+        gsap.to(".carousel_tooltip", 0, {
+            x: cursorX,
+            y: cursorY
+        })
+    })
+
+    gsap.from(".tools_mobile", 1.4, {
+        scrollTrigger: {
+            trigger: '.tools h3',
+            start: 'top 80%'
+        },
+        delay: 0.4,
+        opacity: 0,
+        ease: 'power2'
+    })
+
+    gsap.to("body", {
+        scrollTrigger: {
+            trigger: '.profile_bg',
+            start: 'top top',
+            end: 'top top',
+            scrub: 0.4
+        },
+        backgroundColor: '#000000',
+        duration: 1,
+        ease: 'none'
+    })
+
+    gsap.from("body", {
+        scrollTrigger: {
+            trigger: '.work',
+            start: 'top bottom',
+            end: 'top 98%',
+            scrub: 0.4
+        },
+        backgroundColor: '#000000',
+        duration: 1,
+        ease: 'none'
+    })
+
+    gsap.from(".resume", 1, {
+        scrollTrigger: {
+            trigger: '.resume',
+            start: 'top 85%',
+            end: 'top 70%',
+            scrub: 0.2
+        },
+        opacity: 0,
+        y: '1rem',
+        ease: 'none'
+    })
+
     $('.resume a').mouseover(function(){
         gsap.from(this, 0.6, { delay: 0.3, '--width': 0, ease: 'expo.inOut' })
         gsap.from(this, 0.8, { '--transform': 'scaleX(1)', ease: 'expo.inOut' })
@@ -131,7 +296,7 @@ $(document).ready(function(){
 
     $('.work_content_wrap>div>div').mouseover(function(){
         $(this).find('.cover').stop().fadeOut(300)
-        $(this).find('.info').css({'filter':'none'})
+        $(this).find('.info').css({'filter':'blur(0)'})
     }).mouseout(function(){
         $('.cover').stop().fadeIn(200)
         $(this).find('.info').css({'filter':'blur(20px)'})
@@ -379,6 +544,115 @@ $(document).ready(function(){
         }
     }
     //------------------ CodePen Code ------------------//
+    let carousel = document.querySelector('.carousel');
+    let carouselPivot = carousel.querySelector('.carousel_pivot');
+
+    let carouselRadius = 100;
+    let carouselRotation = 65;
+    let carouselTilt = 0;
+    let carouselVelocity = 10;
+    let carouselIsDragging = false;
+    let carouselDragPosition;
+
+    function start() {
+        let items = carouselPivot.children;
+        let arc = 540 / items.length;
+        for(let i = 0; i<items.length; i++) {
+            let item = items[i];
+            item.style.transform = 'translate3d(-50%, -50%, 0) rotateY(' + (i * arc) + 'deg) translateZ(' + (carouselRadius * -18) + 'px)'
+        }
+    }
+
+    function dragStart(position) {
+        carouselIsDragging = true;
+        carouselVelocity = 0;
+        carouselDragPosition = position.x;
+    }
+
+    function dragMove(position) {
+        carouselVelocity = Math.atan2(position.x - carouselDragPosition, carouselRadius * 2) * -75 / Math.PI;
+        carouselDragPosition = position.x;
+    }
+
+    function dragEnd(position) {
+        carouselIsDragging = false;
+    }
+
+    function update() {
+        carouselRotation += carouselVelocity;
+    
+        if(!carouselIsDragging) {
+            carouselVelocity *= 0.92;
+        }
+        
+        carouselTilt += (carouselVelocity * 0.1 - carouselTilt) / 10;
+        
+        carouselPivot.style.transform = 'translateZ(-'+ carouselRadius * 2 +'px) rotateX(' + -carouselTilt + 'deg) rotateY(' + carouselRotation + 'deg) ';
+    }
+
+    (function(){
+        let location = function(evt) {
+        let t1 = evt.touches, t2 = evt.changedTouches;
+        let s = (t1 && t1[0]) || (t2 && t2[0]) || evt;
+        return {x:s.pageX, y:s.pageY};
+    };
+    
+    let prevent = function(evt) {
+        evt.preventDefault();
+    };
+
+    let handler = function(evt) {
+        switch(evt.type) {
+        case 'mousedown':
+            add(document, ['mousemove', 'mouseup']);
+        case 'mousedown':
+        case 'touchstart':
+            prevent(evt);
+            dragStart(location(evt));
+            break;
+        case 'mousemove':
+        case 'touchmove':
+            dragMove(location(evt));        
+            break;
+        case 'mouseup':
+            remove(document, ['mousemove', 'mouseup']);
+        case 'mouseup':
+        case 'touchend':
+        case 'touchcancel':
+            dragEnd(location(evt));
+            break;
+        }
+    };
+    let add = function(target, events) {
+        for(let i = 0; i<events.length; i++) {
+        target.addEventListener(events[i], handler);
+        }
+    };
+    let remove = function(target, events) {
+        for(let i = 0; i<events.length; i++) {
+        target.removeEventListener(events[i], handler);
+        }
+    };
+    
+    add(carousel, ['mousedown', 'touchstart', 'touchmove', 'touchend', 'touchcancel']);
+    carousel.ondragstart = function() { return false; };
+    })();
+
+    (function() {
+        let timestamp = window.performance ? function(){return window.performance.now()/1000;} : function(){return new Date().getTime()/1000;};
+        let requestFrame = window.requestAnimationFrame || function(callback) {setTimeout(callback, 16);};
+        start();
+        let time = timestamp();
+        let enterFrame = function() {
+            let now = timestamp();
+            let delta = now - time;
+            time = now;
+            update(delta);
+            requestFrame(enterFrame);
+        };
+        requestFrame(enterFrame);
+    })();
+    //------------------ CodePen Code ------------------//
 
     ScrollTrigger.matchMedia({
         "(min-width: 1025px)": function(){
@@ -424,42 +698,6 @@ $(document).ready(function(){
                 ease: 'expo.out'
             })
         },
-
-        "(min-width: 769px)": function(){
-            gsap.to(".about_text h3", 0.2, { opacity: 0.2 })
-            gsap.to(".skill h3", 0.2, { opacity: 1 })
-            $('.about_detail_wrap').stop().fadeOut(100)
-            $('.skill .about_detail_wrap').stop().fadeIn(200)
-            $('.skill .about_detail').addClass('on')
-
-            $('.about_text h3').on('mouseover',function(){
-                if($(this).siblings('div').hasClass('on')){
-                    return
-                }else{
-                    $('.on').removeClass('on')
-                    $(this).siblings('div').addClass('on')
-            
-                    $('.about_detail_wrap').stop().fadeOut(100)
-                    $('.on>.about_detail_wrap').stop().fadeIn(200)
-        
-                    gsap.to(".about_text h3", 0.2, { opacity: 0.2 })
-                    gsap.to(this, 0.2, { opacity: 1 })
-                    gsap.from(".on>.about_detail_wrap", 0.4, { scale: 0.9 })
-                }
-            })
-        },
-
-        "(max-width: 768px)": function(){
-            $('.about_text h3').off('mouseover')
-            .siblings('div').removeClass('on')
-            .find('.about_detail_wrap').stop().fadeIn(200)
-
-            gsap.to(".about_text h3", { opacity: 1 })
-            gsap.to(".education .about_detail_wrap", {
-                'display': 'flex',
-                'justify-content': 'space-between'
-            })
-        }
     })
 })
 
